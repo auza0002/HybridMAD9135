4f74c3053da0f9a378e52079cd0f1a34fb839795
Diego Auza <auza0002@algonquinlive.com>
2023-09-07 19:32:39 -0400

    hybidOne

---
+++ hybrid_one/bin/hybrid_one.dart
@@ -0,0 +1,5 @@
+import 'package:hybrid_one/hybrid_one.dart' as hybrid_one;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hybrid_one.calculate()}!');
+}
---
+++ hybrid_one/test/hybrid_one_test.dart
@@ -0,0 +1,8 @@
+import 'package:hybrid_one/hybrid_one.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
