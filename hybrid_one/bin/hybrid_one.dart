fb5ac1c5f94bd8dbffab95b0997ed6b0a490dda5
Diego Auza <auza0002@algonquinlive.com>
2023-09-07 23:26:26 -0400

    classes moneky and fish

--- hybrid_one/bin/hybrid_one.dart
+++ hybrid_one/bin/hybrid_one.dart
@@ -1,25 +1,52 @@
 import 'package:hybrid_one/hybrid_one.dart' as hybrid_one;
 
 void main(List<String> arguments) {
-  GoldenFish goldenFish = GoldenFish();
+  //
+  GoldenFish goldenFish = GoldenFish("Diego Auza");
+  print("the name of the owner is ${goldenFish.ownerName}");
+  goldenFish.eat("golden fish fast food");
+  goldenFish.swim("hot");
 
-  SilverFish silverFish = SilverFish();
+  //
+  Monkey jorgeMonkey = Monkey();
+  jorgeMonkey.eat("banana");
+  //
 }
 
-class Fish {
-  Fish();
-}
-
-class GoldenFish extends Fish {
-  eat(var food) {}
-  swim() {
-    print("the fish is swimmig");
+class Animal {
+  Animal();
+  eat(var food) {
+    print("The animal said thanks for the $food");
   }
 }
 
-class SilverFish extends Fish {
-  eat() {}
-  reverse() {
-    print("The fish is going backwards");
+class GoldenFish extends Animal {
+  late String ownerName;
+  List<String> waterTemperature = ["cold", "normal"];
+
+  GoldenFish(this.ownerName);
+  @override
+  eat(var food) {
+    print('the fish eats "$food"');
+  }
+
+  swim(var currentTemperature) {
+    if (waterTemperature.contains(currentTemperature)) {
+      print("the golden fish is swimming");
+    } else {
+      print("the temperture is not good for the healht of the fish");
+      die();
+    }
+  }
+
+  die() {
+    print("the fish is not alive any more");
+  }
+}
+
+class Monkey extends Animal {
+  @override
+  eat(food) {
+    print('monkey eats $food');
   }
 }
